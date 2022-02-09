caa15ab9cbca38bcb631e5229744fa5460ee792b
Cofeek <94117043+cofeek-codes@users.noreply.github.com>
2022-02-09 17:03:17 +0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,197 @@
+
+const browserSync = require("browser-sync").create();
+const { src, dest, watch, series, parallel } = require("gulp");
+const fileinclude = require("gulp-file-include");
+const del = require("del");
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require("gulp-sass")(require("sass"));
+const groupCssMediaQueries = require("gulp-group-css-media-queries");
+const shorthand = require("gulp-shorthand");
+const babel = require("gulp-babel");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const fonter = require("gulp-fonter");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const zip = require("gulp-zip")
+
+
+
+
+
+// PLUGINS
+
+// PLUGINS
+
+
+
+// HTML
+
+const watcher = () => {
+    watch("./src/html/**/*.html", html).on("all", browserSync.reload);
+    watch("./src/style/**/*.scss", style).on("all", browserSync.reload);
+    watch("./src/js/**/*.js", javascript).on("all", browserSync.reload);
+    watch("./src/img/*.{jpg,png,jpeg}", images).on("all", browserSync.reload);
+    watch("./src/img/svg/*.svg", svg).on("all", browserSync.reload);
+    watch("./src/fonts/*.*", fonts).on("all", browserSync.reload);
+    watch("./src/img/icons/*.*", icons).on("all", browserSync.reload);
+    watch("./src/img/favicon/*.*", favicon).on("all", browserSync.reload);
+    watch("./dist/**/*.*", zipar).on("all", browserSync.reload);
+
+}
+
+// del
+
+const removedir = () => {
+    return del("./dist");
+}
+
+const html = () => {
+    console.log("HTML");
+    return src("./src/html/*.html")
+
+
+
+
+        .pipe(fileinclude())
+        .pipe(dest("./dist"))
+        .pipe(browserSync.stream());
+
+}
+
+// STYLE
+
+const style = () => {
+    console.log("STYLE");
+    return src("./src/style/*.scss")
+
+
+
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(groupCssMediaQueries())
+        .pipe(shorthand())
+        .pipe(dest("./dist/css"));
+
+
+}
+// JAVASCRIPT
+
+const javascript = () => {
+    console.log("STYLE");
+    return src("./src/js/*.js")
+
+
+
+
+        .pipe(dest("./dist/js"));
+
+
+}
+
+// SERVER
+
+const server = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    })
+}
+
+// FAVICON
+
+const favicon = () => {
+    console.log("favicon");
+    return src("./src/img/favicon/**/*.*")
+
+
+
+
+        .pipe(dest("./dist/src/img/favicon"));
+
+
+}
+
+
+
+
+// IMAGES
+
+const images = () => {
+    console.log("IMAGES");
+    return src("./src/img/*.{jpg,png,jpeg}")
+
+        .pipe(newer("./dist/src/img"))
+        .pipe(dest("./dist/src/img"))
+        .pipe(imagemin({
+            verbose: true
+        }))
+        .pipe(dest("./dist/src/img"))
+}
+
+
+// SVG
+
+const svg = () => {
+    console.log("SVG");
+    return src("./src/img/svg/*.svg")
+        .pipe(newer("./dist/src/img/svg"))
+        .pipe(imagemin({
+            verbose: true
+        }))
+        .pipe(dest("./dist/src/img/svg"))
+}
+
+// FONTS
+
+const fonts = () => {
+    console.log("FONTS");
+    return src("./src/fonts/*.*")
+        .pipe(newer("./dist/src/fonts"))
+        .pipe(fonter({
+            formats: ["ttf", "woff", "eot", "svg"]
+        }))
+        .pipe(dest("./dist/src/fonts"))
+        .pipe(ttf2woff2())
+        .pipe(dest("./dist/src/fonts"))
+}
+
+const icons = () => {
+    console.log("ICONS");
+    return src("./src/img/icons/**/*.*")
+        .pipe(newer("./dist/src/img/icons/*.*"))
+        .pipe(fonter({
+            formats: ["ttf", "woff", "eot", "svg"]
+        }))
+        .pipe(dest("./dist/src/img/icons"))
+        .pipe(ttf2woff2())
+        .pipe(dest("./dist/src/img/icons"))
+}
+
+const zipar = () => {
+    console.log("ZIP");
+    return src("./dist/**/*.*")
+        .pipe(zip('dist.zip'))
+        .pipe(dest("./dist"))
+}
+
+// Build
+exports.html = html;
+exports.style = style;
+exports.javascript = javascript;
+exports.images = images;
+exports.svg = svg;
+exports.fonts = fonts;
+exports.icons = icons;
+exports.favicon = favicon;
+exports.zipar = zipar;
+exports.watch = watcher;
+exports.removedir = removedir;
+exports.dev = series(
+    removedir,
+    parallel(html, style, javascript, images, svg, fonts, icons, favicon),
+    parallel(watcher, server),
+    zipar
+
+);
+
